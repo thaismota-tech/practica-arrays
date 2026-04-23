71e891379d87af163543d3faa3c9a5852771699f
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-23 15:32:43 +0200

    He finalizado el ejercicio 1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Ejercicio 1: Dado un array, retorna el primer elemento del mismo.
+let chocolates = ["negro", "blanco", "conleche", "ruby"];
+console.log(chocolates[0]); 
+
+// Ejercicio 2: Obtener el último elemento del array
+let barrios = ["leme", "copacabana", "arpoador", "ipanema", "leblon"];
+console.log(barrios[4]);
+
+// Ejercicio 3: Agregar un elemento al inicio del array
+let andalucia = ["sevilla", "málaga", "córdoba", "granada", "almería", "huelva", "jaén"];
+andalucia.unshift("cádiz");
+console.log(andalucia);
