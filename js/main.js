707ae7783f57450a3e78f25314e18929ff8d3007
thaismota-tech <thaissantosdamota@gmail.com>
2026-04-23 15:48:50 +0200

    He finalizado el ejercicio 5

--- js/main.js
+++ js/main.js
@@ -2,17 +2,25 @@
 let chocolates = ["negro", "blanco", "conleche", "ruby"];
 console.log(chocolates[0]); 
 
+
 // Ejercicio 2: Obtener el último elemento del array
 let barrios = ["leme", "copacabana", "arpoador", "ipanema", "leblon"];
 console.log(barrios[4]);
+
 
 // Ejercicio 3: Agregar un elemento al inicio del array y lo devuelve actualizado
 let andalucia = ["sevilla", "málaga", "córdoba", "granada", "almería", "huelva", "jaén"];
 andalucia.unshift("cádiz");
 console.log(andalucia);
 
+
 // Ejercicio 4: Eliminar el primer elemento del array
 let hoteles = ["hilton", "windsor", "ibis", "marriott"]
 let primero = hoteles.shift();
 console.log(hoteles);
 
+
+// Ejercicio 5: Agregar un elemento al final del array
+let nombresdechicas = ["kate", "ella", "sophie", "mary", "joy"]
+nombresdechicas.push("hanna");
+console.log(nombresdechicas);
